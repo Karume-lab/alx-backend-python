51b3e1beb605b801e080428429bae804cc3eadda
Karume-lab <danielkarume@outlook.com>
2023-10-05 22:26:02 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Function to_str that casts a float to a string'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Args:
+        n (float): number to type cast
+
+    Returns:
+        str: type casted float to string
+    '''
+    return str(n)
