c7ca61ad89153fbc699d4f36273417d376bbb77e
Karume-lab <danielkarume@outlook.com>
2023-10-05 22:19:07 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Function floor that floors a float'''
+
+
+def floor(n: float) -> int:
+	'''
+	Args:
+		n (float): number to floor
+
+	Returns:
+		int: the floored number
+	'''
+	return int(n)
