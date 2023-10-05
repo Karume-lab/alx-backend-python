186c73861b531c47c1fcc72a050adc23e3f825f7
Karume-lab <danielkarume@outlook.com>
2023-10-05 21:59:30 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a:float, b:float) -> float:
+	"""
+	Function that adds two floats
+
+	Args:
+		a (float): first number
+		b (float): second number
+
+	Returns:
+		float: the sum of the two args
+	"""
+	return a + b
