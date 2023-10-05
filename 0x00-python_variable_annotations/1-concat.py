55d678b7a5b07cb90a8b9c1f93b9c45e0c2822e5
Karume-lab <danielkarume@outlook.com>
2023-10-05 22:11:09 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Takes two strings and returns a concatenated string'''
+
+
+def concat(src: str, dest: str) -> str:
+    '''Concatenates two strings'''
+    return src + dest
